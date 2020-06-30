39d19b1e73e72f459b71dde36e7d16cbd9e6e83c
SHIM-WONBO <59244722+SHIM-WONBO@users.noreply.github.com>
2020-06-30 14:39:03 +0900

    book management system

--- book_main.py
+++ book_main.py
@@ -1,4 +1,4 @@
-import os
+import os, copy, pickle
 from book_management import BM
 
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
@@ -11,26 +11,27 @@ while True:
     book_list.append(tmpbl)
     if not tmpbl: break
 f.close()
-
+del book_list[len(book_list)-1]
 while True:
     bm = BM(book_list)
     print('\n====== 메뉴 번호를 입력하세요 ======')
     number = input("1. 도서 추가 \n2. 도서 검색 \n3. 도서 정보 수정 \n4. 도서 삭제 \n5. 현재 총 도서 목록 출력 \n6. 저장 \n7. 프로그램 나가기 \n입력 : ")
     if number == '1':
-        bm.menu1(book_list)
+        book_list = copy.deepcopy(bm.menu1(book_list))
     elif number == '2':
         bm.menu2(book_list)
     elif number == '3':
-        bm.menu3(book_list)
+        book_list = copy.deepcopy(bm.menu3(book_list))
     elif number == '4':
-        bm.menu4(book_list)
+        book_list = copy.deepcopy(bm.menu4(book_list))
     elif number == '5':
         bm.menu5(book_list)
     elif number == '6':
         bm.menu6(book_list)
     elif number == '7':
-        with open('input.txt', 'w') as file:    
-            file.writelines(book_list)
+        with open('input.txt', 'wb') as file:
+            for i in range(len(book_list)):
+                pickle.dump(book_list[i], file)
         print('프로그램을 종료합니다.')
         break
     else:
--- book_management.py
+++ book_management.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 
 class BM:
     def __init__(self, book_list):
@@ -7,6 +8,7 @@ class BM:
     def menu1(self,book_list):
         newbook_list = list(input("도서명, 저자, 출판연도, 출판사명, 장르 입력(빈칸으로 구분)\n").split())
         book_list.append(newbook_list)
+        return book_list
         # 1 도서 추가
 
     def menu2(self,book_list):
@@ -19,7 +21,7 @@ class BM:
 
     def menu3(self,book_list):
         i = 0
-        while i < (len(book_list)-1):
+        while i < (len(book_list)):
             a,b,c,d,e = book_list[i]
             print(i+1,end='.')
             print(a,b,c,d,e)
@@ -27,22 +29,24 @@ class BM:
         num = int(input('수정할 도서의 목록번호를 입력하세요 : '))
         mdy_list = list(input('도서명, 저자, 출판연도, 출판사명, 장르 입력(빈칸으로 구분)\n').split())
         book_list[num-1] = mdy_list.copy()
+        return book_list
         # 3 도서 수정
 
     def menu4(self,book_list):
         i = 0
-        while i < (len(book_list)-1):
+        while i < (len(book_list)):
             a,b,c,d,e = book_list[i]
             print(i+1,end='.')
             print(a,b,c,d,e)
             i += 1
         num = int(input('삭제할 도서의 목록번호를 입력하세요 : '))
         book_list.pop(num-1)
+        return book_list
         # 4 도서 수정
 
     def menu5(self,book_list):
         i = 0
-        while i < (len(book_list)-1):
+        while i < (len(book_list)):
             a,b,c,d,e = book_list[i]
             print(i+1,end='.')
             print(a,b,c,d,e)
@@ -50,6 +54,7 @@ class BM:
         # 5 도서 목록 보기
 
     def menu6(self,book_list):
-        with open('input.txt', 'w') as file:    
-            file.writelines(book_list)
+        with open('input.txt', 'wb') as file:
+            for i in range(len(book_list)):
+                pickle.dump(book_list[i], file)
         # 6 도서목록 저장
