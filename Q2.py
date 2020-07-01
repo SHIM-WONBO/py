aae7508d4e0294eeb77d92e91468cc8df4bff099
SHIM-WONBO <59244722+SHIM-WONBO@users.noreply.github.com>
2020-07-01 16:47:33 +0900

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -12,17 +12,19 @@ while joblist[m] < tst_largest:     # 입력받은 값이 큐의 최대값보다
     elif tstlist[0] < tst_largest:  # 큐의 첫번째 값이 최대값보다 작으면 그 값을 맨 뒤로 보냄
         tstlist.append(tstlist[0])
         tstlist.pop(0)
-
     tst_largest = max(tstlist)
 
-while True:
-    if joblist[len(joblist)-1] == joblist[m]:
-        minute += 1
-        joblist.pop()
-    elif joblist[len(joblist)-1] < joblist[m]:
-        joblist.pop()
-    elif joblist[len(joblist)-1] > joblist[m]:
-        break
+if joblist.count(joblist[m]) == 1:
+    pass
+elif joblist.count(joblist[m]) > 1:
+    while True:
+        if joblist[len(joblist)-1] == joblist[m]:
+            minute += 1
+            joblist.pop()
+        elif joblist[len(joblist)-1] < joblist[m]:
+            joblist.pop()
+        elif joblist[len(joblist)-1] > joblist[m]:
+            break
 
 for i in range(0,m+1):
     if joblist[i] == joblist[m]:
